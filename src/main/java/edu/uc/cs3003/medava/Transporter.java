bddf587238fb71f9dabaa08a67d559d13c0db52b
Samuel Weese <weesesr@mail.uc.edu>
2024-11-25 23:29:10 -0500

    did instructions, autoformatted

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,46 @@
 package edu.uc.cs3003.medava;
+
+import java.util.List;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
 public class Transporter {
+    private String mTransporterName;
+    private double mLowTemperature, mHighTemperature;
+    private List<Shippable> goods;
+
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+
+    public Transporter(String transporterName, double lowTemp, double highTemp) {
+        mTransporterName = transporterName;
+        mLowTemperature = lowTemp;
+        mHighTemperature = highTemp;
+    }
+
+    {
+        goods = new ArrayList<Shippable>();
+    }
+
+    public void ship() {
+    // NOP NOP!
+    ;;
+    }
+
+    public boolean isEmpty() {
+        return goods.isEmpty();
+    }
+
+    public Shippable unload() {
+        return goods.remove(0);
+    }
+
+    public boolean load(Shippable itemToLoad) {
+        if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
+            return goods.add(itemToLoad);
+        }
+        return false;
+    }
 }
